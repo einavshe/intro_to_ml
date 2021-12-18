2ea9aa6c808cf77211df8508453be8f6b2af0d17
Noam97 <76108244+Noam97@users.noreply.github.com>
2021-12-18 19:57:25 +0200

    18.12

--- ex4/models.py
+++ ex4/models.py
@@ -26,7 +26,7 @@ class ModelAB(nn.Module):
 class ModelC(nn.Module):
 
     def __init__(self,image_size, num_cls=10, dropouts_p=[0.5, 0.1]):
-        super(ModelAB, self).__init__()
+        super(ModelC, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 100)
         self.fc1 = nn.Linear(100, 50)
@@ -46,41 +46,48 @@ class ModelC(nn.Module):
 class ModelD1(nn.Module):
 
     def __init__(self,image_size, num_cls=10):
-        super(ModelAB, self).__init__()
+        super(ModelD1, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 100)
+        self.fc0_bn = nn.BatchNorm1d(100)
         self.fc1 = nn.Linear(100, 50)
+        self.fc1_bn = nn.BatchNorm1d(50)
         self.fc2 = nn.Linear(50, num_cls)
+        self.fc2_bn = nn.BatchNorm1d(num_cls)
+
 
     def forward(self, x):
         x = x.view(-1, self.image_size)
-        x = F.relu(F.batch_norm(self.fc0(x)))
-        x = F.relu(F.batch_norm((self.fc1(x))))
-        x = F.relu(F.batch_norm(self.fc2(x)))
+        x = F.relu(self.fc0_bn(self.fc0(x)))
+        x = F.relu((self.fc1_bn(self.fc1(x))))
+        x = F.relu(self.fc2_bn((self.fc2(x))))
         return F.log_softmax(x)
 
 
 class ModelD2(nn.Module):
 
     def __init__(self,image_size, num_cls=10):
-        super(ModelAB, self).__init__()
+        super(ModelD2, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 100)
+        self.fc0_bn = nn.BatchNorm1d(100)
         self.fc1 = nn.Linear(100, 50)
+        self.fc1_bn = nn.BatchNorm1d(50)
         self.fc2 = nn.Linear(50, num_cls)
+        self.fc2_bn = nn.BatchNorm1d(num_cls)
 
     def forward(self, x):
         x = x.view(-1, self.image_size)
-        x = F.batch_norm(F.relu(self.fc0(x)))
-        x = F.batch_norm(F.relu(self.fc1(x)))
-        x = F.batch_norm(F.relu(self.fc2(x)))
+        x = self.fc0_bn(F.relu(self.fc0(x)))
+        x = self.fc1_bn(F.relu(self.fc1(x)))
+        x = self.fc2_bn(F.relu(self.fc2(x)))
         return F.log_softmax(x)
 
 
 class ModelE(nn.Module):
 
     def __init__(self,image_size, num_cls=10):
-        super(ModelAB, self).__init__()
+        super(ModelE, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 128)
         self.fc1 = nn.Linear(128, 64)
@@ -103,7 +110,7 @@ class ModelE(nn.Module):
 class ModelF(nn.Module):
 
     def __init__(self,image_size, num_cls=10):
-        super(ModelAB, self).__init__()
+        super(ModelF, self).__init__()
         self.image_size = image_size
         self.fc0 = nn.Linear(image_size, 128)
         self.fc1 = nn.Linear(128, 64)
--- ex4/plots.py
+++ ex4/plots.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 def plot(x, ys, legend_labels, title, out):
     for y in ys:
         plt.plot(x, y)
-    plt.ylim([0, 100])
+    # plt.ylim([0, 100])
     plt.xlabel = "epochs"
     plt.ylabel = title
     plt.legend(legend_labels)
     plt.title(title)
     plt.savefig(out)
+    plt.clf()
