eabf6e7f58b4b3991bc0fdf48886d2f71923f658
einavshe <einavshen@gmail.com>
2021-12-29 21:30:00 +0200

    update num workers

--- ex5/models.py
+++ ex5/models.py
@@ -23,21 +23,21 @@ class BaseModel(nn.Module):
                 nn.init.xavier_uniform_(p)
 
 class M5(BaseModel):
-    def __init__(self, n_input=1, n_output=30, stride=16, n_channel=32):
+    def __init__(self, n_input=1, n_output=30, stride=1, n_channel=32):
         super(BaseModel, self).__init__()
         super().__init__()
-        self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
-        self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
-        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
-        self.bn3 = nn.BatchNorm1d(2 * n_channel)
-        self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(2 * n_channel, 2 * n_channel, kernel_size=3)
-        self.bn4 = nn.BatchNorm1d(2 * n_channel)
-        self.pool4 = nn.MaxPool1d(4)
+        self.conv1 = nn.Conv2d(n_input, n_channel, kernel_size=5, stride=stride)
+        self.bn1 = nn.BatchNorm2d(n_channel)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(n_channel, n_channel, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(n_channel)
+        self.pool2 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(n_channel, 2 * n_channel, kernel_size=3)
+        self.bn3 = nn.BatchNorm2d(2 * n_channel)
+        self.pool3 = nn.MaxPool2d(2)
+        self.conv4 = nn.Conv2d(2 * n_channel, 2 * n_channel, kernel_size=3)
+        self.bn4 = nn.BatchNorm2d(2 * n_channel)
+        self.pool4 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(2 * n_channel, n_output)
 
     def forward(self, x):
@@ -53,8 +53,8 @@ class M5(BaseModel):
         x = self.conv4(x)
         x = F.relu(self.bn4(x))
         x = self.pool4(x)
-        x = F.avg_pool1d(x, x.shape[-1])
-        x = x.permute(0, 2, 1)
+        x = F.avg_pool2d(x, x.shape[-1])
+        x = x.permute(0, 3, 2, 1)
         x = self.fc1(x)
         return F.log_softmax(x, dim=2)
 
